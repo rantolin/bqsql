e65f06a2775187d762bbfbd05f3a79003130a4a2
Roberto Antolin <rantolin.geo@gmail.com>
2024-05-10 14:03:14 +0200

    Add query command

---
+++ cmd/query.go
@@ -0,0 +1,84 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"cloud.google.com/go/bigquery"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"google.golang.org/api/iterator"
+)
+
+// queryBasic demonstrates issuing a query and reading results.
+func queryBasic(w io.Writer, projectID string, query string) error {
+	ctx := context.Background()
+	client, err := bigquery.NewClient(ctx, projectID)
+	if err != nil {
+		return fmt.Errorf("bigquery.NewClient: %v", err)
+	}
+	defer client.Close()
+
+	fmt.Println("query: ", query)
+	q := client.Query(query)
+
+	// Location must match that of the dataset(s) referenced in the query.
+	q.Location = "US"
+
+	// Run the query and print results when the query job is completed.
+	job, err := q.Run(ctx)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	status, err := job.Wait(ctx)
+	if err != nil {
+		return err
+	}
+	if err := status.Err(); err != nil {
+		return err
+	}
+	it, err := job.Read(ctx)
+	for {
+		var row []bigquery.Value
+		err := it.Next(&row)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		fmt.Fprintln(w, row)
+	}
+	return nil
+}
+
+// queryCmd represents the query command
+var queryCmd = &cobra.Command{
+	Use:   "query",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		query := args[0]
+		queryBasic(
+			cmd.OutOrStdout(),
+			viper.GetString("project_id"),
+			query,
+		)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(queryCmd)
+	queryCmd.SetArgs([]string{"query"})
+}
