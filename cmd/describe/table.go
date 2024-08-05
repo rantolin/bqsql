5c55457fa270e013c4561898bc2df43556367647
Roberto Antolin <rantolin.geo@gmail.com>
2024-08-05 10:00:14 +0200

    Refactor: Stop using viper to manage specific flags

--- cmd/describe/table.go
+++ cmd/describe/table.go
@@ -6,7 +6,6 @@ package describe
 import (
 	"context"
 	"fmt"
-	"io"
 
 	"cloud.google.com/go/bigquery"
 	"github.com/spf13/cobra"
@@ -15,14 +14,18 @@ import (
 
 // printTableInfo demonstrates fetching metadata from a table and printing some basic information
 // to an io.Writer.
-func printTableInfo(w io.Writer, projectID, datasetID, tableID string) error {
+func printTableInfo(cmd *cobra.Command, tableID string) error {
 	ctx := context.Background()
+	w := cmd.OutOrStdout()
+
+	projectID := viper.GetString("project_id")
 	client, err := bigquery.NewClient(ctx, projectID)
 	if err != nil {
 		return fmt.Errorf("bigquery.NewClient: %w", err)
 	}
 	defer client.Close()
 
+	datasetID := viper.GetString("dataset")
 	meta, err := client.Dataset(datasetID).Table(tableID).Metadata(ctx)
 	if err != nil {
 		return err
@@ -31,12 +34,14 @@ func printTableInfo(w io.Writer, projectID, datasetID, tableID string) error {
 	fmt.Fprintf(w, "\nTable: %s\n", meta.FullID)
 	fmt.Fprintf(w, "Description: %s\n", meta.Description)
 
-	if viper.GetBool("num_rows") {
+	numRows, _ := cmd.Flags().GetBool("num_rows")
+	if numRows {
 		fmt.Fprintf(w, "Rows in managed storage: %d\n", meta.NumRows)
 	}
 
 	// Print basic information about the table's columns.
-	if viper.GetBool("schema") {
+	schema, _ := cmd.Flags().GetBool("schema")
+	if schema {
 		var columns_format string = "%-20s %-10s\n"
 		fmt.Fprintf(w, "\n"+columns_format, "column_name", "type")
 		for _, fieldSchema := range meta.Schema {
@@ -48,9 +53,9 @@ func printTableInfo(w io.Writer, projectID, datasetID, tableID string) error {
 
 // tableCmd represents the table command
 var tableCmd = &cobra.Command{
-    Use:   "table",
-    Short: "Describes a BigQuery table",
-    Long: `The 'table' subcommand provides detailed information about a specified BigQuery table.
+	Use:   "table",
+	Short: "Describes a BigQuery table",
+	Long: `The 'table' subcommand provides detailed information about a specified BigQuery table.
 You need to provide the project ID, dataset, and table as arguments.
 The command connects to the BigQuery client, retrieves the metadata of the table, and prints it.
 You can also use flags to get specific information like the number of rows (--num_rows) or the schema (--schema).
@@ -58,11 +63,7 @@ It's a convenient way to quickly inspect the details of your BigQuery tables wit
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("table called")
 		table := args[0]
-		projectID := viper.GetString("project_id")
-		datasetID := viper.GetString("dataset")
-		printTableInfo(
-			cmd.OutOrStdout(), projectID, datasetID, table,
-		)
+		printTableInfo(cmd, table)
 	},
 }
 
@@ -71,7 +72,4 @@ func init() {
 
 	tableCmd.Flags().BoolP("num_rows", "n", false, "Show number of rows")
 	tableCmd.Flags().BoolP("schema", "s", false, "Show schema")
-
-	viper.BindPFlag("num_rows", tableCmd.Flags().Lookup("num_rows"))
-	viper.BindPFlag("schema", tableCmd.Flags().Lookup("schema"))
 }
